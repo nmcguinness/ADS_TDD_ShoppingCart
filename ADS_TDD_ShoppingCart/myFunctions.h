9a31ea3391b9f0fe5d74ea34b410e6becfcf1dee
Niall McGuinness <niall.mcguinness@dkit.ie>
2022-10-19 12:39:52 +0100

    Removed print functions

--- ADS_TDD_ShoppingCart/myFunctions.h
+++ ADS_TDD_ShoppingCart/myFunctions.h
@@ -10,27 +10,3 @@ int add(int x, int y)
 {
 	return x + y;
 }
-
-/// @brief Prints a list containing any type (e.g. int, float, bool, string, Point2D, BackAccount etc)
-/// @tparam E
-/// @param list
-template <typename E>
-void print(list<E> list)
-{
-	for (const E& theObj : list)
-		cout << theObj << endl;
-}
-
-/// @brief Prints any data structure that can provide an iterator to the start and end of the structure
-/// @tparam Iter
-/// @param iter
-/// @param end
-template <typename Iter>
-void print(Iter iter, Iter end)
-{
-	while (iter != end)
-	{
-		cout << *iter << endl;
-		iter++;
-	}
-}
